fa450ff883a73ef46ed7d5e6d0aa82fecffba5ca
Seigh sword <zack.yt.7085@gmail.com>
2026-03-05 09:46:21 +0530

    Create keys.py

---
+++ wb-engine/keys.py
@@ -0,0 +1,11 @@
+import os
+
+class Keys:
+    API_ID = int(os.environ.get("TG_API_ID", 0))
+    API_HASH = os.environ.get("TG_API_HASH", "")
+    BOX_ID = int(os.environ.get("BOX_ID", -1003391436065))
+    SESSION_STRING = os.environ.get("SESSION_STRING", "")
+
+    @classmethod
+    def get_session(cls):
+        return cls.SESSION_STRING
