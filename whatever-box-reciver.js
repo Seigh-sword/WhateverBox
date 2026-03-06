a006ad46802ed214c226e18a2641a8df71d0b270
Seigh sword <zack.yt.7085@gmail.com>
2026-03-06 17:54:15 +0530

    Update whatever-box-reciver.js

--- whatever-box-reciver.js
+++ whatever-box-reciver.js
@@ -1,12 +1,26 @@
 const WB_Receiver = {
-    async sendToEngine(endpoint, formData, isJson = false) {
-        const options = {
-            method: isJson ? 'DELETE' : 'POST',
-            body: isJson ? JSON.stringify(formData) : formData
-        };
-        if (isJson) options.headers = { 'Content-Type': 'application/json' };
+    async sendToEngine(endpoint, formData, isJson = false, attempt = 1) {
+        try {
+            const options = {
+                method: isJson ? 'DELETE' : 'POST',
+                body: isJson ? JSON.stringify(formData) : formData
+            };
+            if (isJson) options.headers = { 'Content-Type': 'application/json' };
 
-        const response = await fetch(`http://localhost:8080/api/v1/${endpoint}`, options);
-        return response.json();
+            const response = await fetch(`https://whateverbox.onrender.com/api/v1/${endpoint}`, options);
+            
+            if (!response.ok && attempt <= 5) {
+                throw new Error("Server warming up...");
+            }
+            
+            return await response.json();
+        } catch (err) {
+            if (attempt <= 5) {
+                let delay = attempt * 2000;
+                await new Promise(r => setTimeout(r, delay));
+                return this.sendToEngine(endpoint, formData, isJson, attempt + 1);
+            }
+            throw err;
+        }
     }
 };
